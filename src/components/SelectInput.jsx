988bb203e7db33fb6afc653042ce23b6476c5bce
Francis MudzungayirI <fmudzungayiri@gmail.com>
2025-11-13 11:45:36 +0200

    Save local changes before merge

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,8 +1,9 @@
 const SelectInput = ({ label, options }) => {
   return (
     <div className="mb-6">
+      LUE
       <label className="block text-gray-700 font-medium mb-2">{label}</label>
-      <select className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-[#003366] focus:outline-none">
+      <select className="w-full bg-transparent placeholder:text-LIGHTGRAY text-DARKBLUE text-sm border border-LIGHTGRAY rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
         {options.map((option, index) => {
           return <option key={index}>{option}</option>;
         })}
