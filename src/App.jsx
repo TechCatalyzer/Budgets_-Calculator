7500de4442e2ef67d98254dbb198ca4794dd2adb
Tawa <nyamandwetawanda01@gmail.com>
2025-12-16 09:19:04 +0200

    Refactored the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import JourneyDetails from "./pages/JourneyDetails";
+import LoadingScreen from "./Pages/LoadingScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<JourneyDetails />} />
+        <Route path="/" element={<LoadingScreen />} />
+
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/loading" element={<LoadingScreen />} />
+        <Route path="/journey-details" element={<JourneyDetails />} />
+
       </Routes>
     </BrowserRouter>
   );
